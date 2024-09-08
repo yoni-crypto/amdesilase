a42d60a188039723a0cc10d842a1c433e69a01d2
yoni-crypto <117802287+yoni-crypto@users.noreply.github.com>
2024-09-08 10:08:17 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,33 +7,22 @@ const adminRoutes = require('./routes/adminRoutes');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://amdesilase.vercel.app'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
+  origin: 'https://amdesilase.vercel.app', // Allow your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true, // Allow credentials
   allowedHeaders: ['Content-Type', 'Authorization'], // Allow necessary headers
 }));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://amdesilase.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200); // Respond to preflight request
-  } else {
-    next(); // Pass to the next middleware
-  }
-});
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 const PORT = process.env.PORT || 5000;
-// const MONGO_URI = 'mongodb+srv://yonidisu111:yoniyoye1.@amdeselase.bdsua.mongodb.net/?retryWrites=true&w=majority&appName=amdeselase';
 const MONGO_URI = 'mongodb+srv://amdesilase6:amdesilase0721@amdesilase.q6vuq.mongodb.net/?retryWrites=true&w=majority&appName=amdesilase';
 
-mongoose.connect(MONGO_URI).then(() => console.log('MongoDB connected'))
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
 app.use('/api/students', studentRoutes);
