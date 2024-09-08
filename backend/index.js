42a71d5f77c047a90634afda8529c4a7b8a8e142
yoni-crypto <117802287+yoni-crypto@users.noreply.github.com>
2024-09-08 09:29:11 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://amdesilase.vercel.app'],
+  origin: ['http://localhost:3000','https://amdesilase.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
