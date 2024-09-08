f486e1bde321a6e6cfc21e3351abdeff6ca0e842
yoni-crypto <117802287+yoni-crypto@users.noreply.github.com>
2024-09-08 10:58:40 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,24 +10,12 @@ const app = express();
 
 // CORS middleware
 app.use(cors({
-  origin: '*', // Allow your frontend URL
+  origin: 'https://amdesilase.vercel.app', // Allow your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true, // Allow credentials (e.g., cookies)
   allowedHeaders: ['Content-Type', 'Authorization'], // Allow necessary headers
 }));
 
-// Handle preflight OPTIONS requests globally
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://amdesilase.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
-  res.header('Access-Control-Allow-Credentials', 'true'); // Allow credentials
-
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200); // Send 200 OK for OPTIONS requests (preflight)
-  }
-  next(); // Pass other requests to the next middleware
-});
 
 // Middleware
 app.use(bodyParser.json());
